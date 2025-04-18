259138dca5476730ef023ccdb54a40e9ea874c5a
Becca Cohen <145156789+rscohen2@users.noreply.github.com>
2025-04-18 11:24:46 -0500

    adding notes from class about solver

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 from generate_puzzle import generate_unsolved_puzzle
 from generate_puzzle import get_clues
 import numpy as np
+import random
 
 class Game:
     def __init__(self):
@@ -34,6 +35,7 @@ def check_clue(x,y, grid, clue_grid):
     rows, cols = grid.shape
     if grid[x, y] != 'l' and grid[x, y] != 't':
         cell_clue = 0
+        empty_neighbors_count = 0
         directions = {
             'cell_right': (x + 1, y),
             'cell_left': (x - 1, y),
@@ -44,15 +46,55 @@ def check_clue(x,y, grid, clue_grid):
                 neighbor_value = grid[nx, ny]
                 if neighbor_value != ' ':
                     cell_clue += cell_value(neighbor_value)
-                    if cell_clue == clue_grid[x,y]:
-                        return True
-                    else:
-                        return False
+                else:
+                    empty_neighbors_count += 1
 
-# def brute_force(grid, puzzle):
-#     grid = np.array(grid)
-#     rows, cols = grid.shape
-#
+                if cell_clue == clue_grid[x, y]:
+                    return True
+                    # if cell_clue + 1*empty_neighbors_count or cell_clue -1*empty_neighbors_count == clue_grid[x, y]:
+                    #     return True
+                if cell_clue + generate_permutation(empty_neighbors_count) == clue_grid[x, y]:
+                    return True
+                return False
+
+
+
+def generate_permutation(n):
+    # Choose how many -1s and 1s you want (e.g., equal split)
+    half = n // 2
+    remainder = n % 2
+
+    values = [-1] * half + [1] * half
+
+    if remainder:  # If n is odd, add one more randomly
+        values.append(random.choice([-1, 1]))
+
+    random.shuffle(values)  # Shuffle to get a random permutation
+    return values
+
+# Example usage
+permutation = generate_permutation(7)
+print(permutation)
+
+
+def brute_force(grid, puzzle):
+    grid = np.array(grid)
+    rows, cols = grid.shape
+
+    for x in range(rows):
+        for y in range(cols):
+            if grid[x, y] == 'e':
+                grid[x][y] = 't'
+                if check_clue(x, y, grid, puzzle):
+                    continue
+                else:
+                    grid[x][y] = 'l'
+                    if check_clue(x, y, grid, puzzle):
+                        continue
+                    else:
+                        break
+    return grid
+
     #all permutations that might possibly work
     #predictable/simple sequence to try, so that any backtracking is as simple as possible also
     #if layout not valid at all reject the whole thing, if not possible to get that clue
