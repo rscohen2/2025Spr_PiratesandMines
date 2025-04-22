7b4c1fbc6509357d5547ecdd41352295f98b0fe3
Markkkx <1536222503@qq.com>
2025-04-22 15:29:57 -0500

    add a run_game file to interactively generate the puzzle and the solution

---
+++ run_game.py
@@ -0,0 +1,13 @@
+from generate_puzzle import PuzzleGenerator
+import numpy as np
+
+
+if __name__ == "__main__":
+    grid_size = int(input("Enter the grid size (n x n): "))
+    difficulty = input("Enter the difficulty (easy, medium, hard): ").lower()
+    generator = PuzzleGenerator(size = grid_size, difficulty = difficulty)
+    puzzle, solution = generator.create_puzzle()
+    print("Puzzle:")
+    print(np.matrix(puzzle))
+    # print("\nSolution:")
+    # print(np.matrix(solution))
